b406b645a3ed496180a83f9157fade29503a211f
imNovoa <43404872+imNovoa@users.noreply.github.com>
2018-10-31 11:22:06 +0100

    Reorganización de carpetas

---
+++ Versiones antiguas/Racing Thieves 1.5/src/states/preload.js
@@ -0,0 +1,38 @@
+CatCatcher.preloadState = function(game) {
+
+}
+
+CatCatcher.preloadState.prototype = {
+
+    preload: function() {
+        this.load.image('background', '../assets/images/bg.png');
+        this.load.image('cat', '../assets/images/cat.png');
+        this.load.image('catcher', '../assets/images/catcher.png');
+        this.load.image('block', 'assets/images/block.png');
+        this.load.image('back', 'assets/images/background2.png');
+        this.load.spritesheet('fireball', 'assets/images/FireBall.png',60,29);
+        this.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
+        this.load.tilemap('mario', 'assets/maps/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('tiles', 'assets/tiles/super_mario.png');
+        this.load.image('3', 'assets/images/skill.png');
+        this.load.image('box', 'assets/images/box.png');
+        this.load.image('2', 'assets/images/greenbar2.png');
+        this.load.image('1', 'assets/images/greenbar.png');
+
+        this.load.audio('lvl', 'assets/audio/lv.mp3');
+        this.load.audio('menu', 'assets/audio/menu.mp3');
+    },
+
+    create: function () {
+        var text = "LOADING";
+        var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+
+        var t = game.add.text(game.world.centerX - 300, 0, text, style);
+
+        this.state.start('title');
+    },
+
+    update: function() {
+
+    }
+}
---
+++ Versiones antiguas/Racing Thieves 1.5/src/states/title.js
@@ -0,0 +1,48 @@
+CatCatcher.titleState = function(game) {
+}
+
+
+function over(object) {
+    object.fill = "#ffff44";
+}
+
+function out(object) {
+    object.fill = "#ff0044";
+}
+
+function nextState(object){
+    this.state.start(object.ns);
+}
+
+
+
+CatCatcher.titleState.prototype = {
+
+    preload: function () {
+
+    },
+
+
+    create: function() {
+        var text = "RACING THIEVES";
+        var play = "PLAY";
+        var style = { font: "65px Impact", fill: "#ff0044", align: "center",boundsAlignH: "center", boundsAlignV: "middle" };
+
+        var t = game.add.text(0, 0, text, style);
+        var p = game.add.text(0,0, play, style);
+
+        t.setTextBounds(0, 100, 800, 100);
+        p.setTextBounds(0, 400, 800, 100);
+
+        p.ns = 'menu';
+
+        p.inputEnabled = true;
+
+        p.events.onInputOver.add(over, this);
+        p.events.onInputOut.add(out, this);
+        p.events.onInputDown.add(nextState, this)
+
+    },
+
+
+}
